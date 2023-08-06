e9db7e69b7fdb2bfb01f48fc04c178517c33ed5d
Olaide Hannah <118212140+Hollannah@users.noreply.github.com>
2023-08-06 13:33:31 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""This is the file storage class for AirBnB"""
 import json
+
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -11,58 +12,64 @@ from models.user import User
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
     Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
+        __file_path: path to the JSON file
+        __objects: objects will be stored
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Return a dictionary of instantiated objects in __objects.
-        If a cls is specified, returns a dictionary of objects of that type.
-        Otherwise, returns the __objects dictionary.
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
         """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
-        return self.__objects
+        if cls is None:
+            return self.__objects.copy()
+        else:
+            return {key: obj
+                    for key, obj in self.__objects.items()
+                    if type(obj) is cls}
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id."""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def delete(self, obj=None):
+        """delete obj from __objects
+        """
+        if isinstance(obj, BaseModel):
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
+        """serialize data to the JSON file
+        """
+        new_dict = {key: obj.to_dict()
+                    for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(new_dict, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """deserialize data from the JSON file
+        """
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
-
     def close(self):
-        """Call the reload method."""
+        """alias for reload
+        """
         self.reload()
